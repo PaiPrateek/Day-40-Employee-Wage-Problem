0669cf7b97a0c53f4c978f1f6bf4f78225ab8a46
Prateek <prateekvasanthpai@gmail.com>
2022-07-30 17:48:29 +0530

    Employee id & salary are non zero positive number, gender is M or F, date ois not future date

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -695,7 +695,7 @@ console.log(newEmployeeObject.toString());
 */
 //UC13
 //Check Name using Regex 
-
+/*
 class EmployeePayRollData{
     //Properties
     id;
@@ -739,3 +739,66 @@ catch(ex){
 }
 let newEmployeeObject = new EmployeePayRollData(2,'Vasanth',200000,'M',new Date());
 console.log(newEmployeeObject.toString());
+*/
+
+//Uc14 
+//Employee id & salary are non zero positive number, gender is M or F, date ois not future date
+class EmployeePayRollData{
+    //Properties
+    id;
+    salary;
+
+    //Constructor
+    constructor(...params){
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
+    }
+
+    //Getter & Setter Methods
+    get name() {return this._name};
+    set name(name) {
+        let namePattern = new RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (namePattern.test(name)) this._name = name;
+        else throw 'Name Pattern is incorrect';
+    }
+    get gender() {return this._gender};
+    set gender(gender){
+        let genderPattern = new RegExp('^[F,M]{1}');
+        if (genderPattern.test(gender)) this._gender = gender;
+        else throw 'Gender Pattern is incorrect';
+    }
+    get startDate() {return this._startDate};
+    set startDate(startDate){
+        if (startDate <= new Date()) this._startDate = startDate;
+        else throw 'Date can not be of future';
+    }
+
+    //Method
+    toString(){
+        const options = {year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = this.startDate === undefined ? "undefined" :
+                        this.startDate.toLocaleDateString("en-US", options);
+        return "ID = "+ this.id + " Name = "+ this.name + " Salary = " + this.salary + " Gender : "+ this.gender+ " StartDate : "+ this.startDate;
+    }
+}
+let employeeObject1 = new EmployeePayRollData(1,'Prateek',200000,'M',new Date());
+console.log(employeeObject1.toString());
+try{
+    let employeeObject = new EmployeePayRollData(2,'Ramanath',200000,'S',new Date());
+    console.log(employeeObject.toString());
+}
+catch (ex){
+    console.error(ex);
+}
+finally{
+    try{
+        let newEmployeeObject = new EmployeePayRollData(3,'Vasanth',200000,'F',new Date(2022,3,7,12,12,12,12));
+        console.log(newEmployeeObject.toString());
+    }
+    catch (ex){
+        console.error(ex);
+    }
+}
