467471ebd8c0dd61865b11fecf592a6b131131f8
Prateek <prateekvasanthpai@gmail.com>
2022-07-30 17:39:30 +0530

    Store Gender and Start Date

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -620,9 +620,10 @@ let nonWorkingDayNums = dailyEmpWageArray.filter(dailyHrsandWage=> dailyHrsandWa
 console.log("\nUC 11D NonWorkingDayString : " + nonWorkingDayNums);
 */
 
+// UC11
 // Employee Payroll Class
-//UC11 Creating Employee Payroll Class
-//
+//Creating Employee Payroll Class
+/*
 class EmployeePayRollData{
     //Properties
     id;
@@ -650,3 +651,43 @@ class EmployeePayRollData{
 }
 let employeeObject = new EmployeePayRollData(1,'Prateek',200000);
 console.log(employeeObject.toString());
+*/
+
+//UC12
+//Store Gender and Start Date
+class EmployeePayRollData{
+    //Properties
+    id;
+    salary;
+    gender;
+    startDate
+
+    //Constructor
+    constructor(...params){
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
+    }
+
+    //Getter & Setter Methods
+    get name() {return this._name};
+    set name(name) {
+        let namePattern = new RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (namePattern.test(name)) this._name = name;
+        else throw 'Name is incorrect';
+    }
+
+    //Method
+    toString(){
+        const options = {year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = this.startDate === undefined ? "undefined" :
+                        this.startDate.toLocaleDateString("en-US", options);
+        return "ID = "+ this.id + " Name = "+ this.name + " Salary = " + this.salary + " Gender : "+ this.gender+ " StartDate : "+ this.startDate;
+    }
+}
+let employeeObject = new EmployeePayRollData(1,'Prateek',200000);
+console.log(employeeObject.toString());
+let newEmployeeObject = new EmployeePayRollData(2,'Ramanath',200000,'M',new Date());
+console.log(newEmployeeObject.toString());
