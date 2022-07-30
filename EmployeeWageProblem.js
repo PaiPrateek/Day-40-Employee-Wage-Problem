443ee33e37e2447a975d2a2f1e39bf2f661d16a3
Prateek <prateekvasanthpai@gmail.com>
2022-07-30 17:34:14 +0530

    Creating Employee Payroll Class

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -537,6 +537,7 @@ function GetDailyWage(empHrs) {
 
 //UC11
 //Object Operation using arrow functions
+/*
 //CONSTANTS
 const IS_FULL_TIME = 2;
 const IS_ABSENT = 0;
@@ -617,3 +618,35 @@ console.log("\nUC 11C PartWorkingDayString : " + partWorkingDayStrArr);
 let nonWorkingDayNums = dailyEmpWageArray.filter(dailyHrsandWage=> dailyHrsandWage.dailyHours==0)
 .map(dailyHrsandWage=> dailyHrsandWage.toString());
 console.log("\nUC 11D NonWorkingDayString : " + nonWorkingDayNums);
+*/
+
+// Employee Payroll Class
+//UC11 Creating Employee Payroll Class
+//
+class EmployeePayRollData{
+    //Properties
+    id;
+    salary;
+
+    //Constructor
+    constructor(id,name,salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary
+    }
+
+    //Getter & Setter Methods
+    get name() {return this._name};
+    set name(name) {
+        let namePattern = new RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (namePattern.test(name)) this._name = name;
+        else throw 'Name is incorrect';
+    }
+
+    //Method
+    toString(){
+        return "ID = "+ this.id + " Name = "+ this.name + " Salary = " + this.salary;
+    }
+}
+let employeeObject = new EmployeePayRollData(1,'Prateek',200000);
+console.log(employeeObject.toString());
